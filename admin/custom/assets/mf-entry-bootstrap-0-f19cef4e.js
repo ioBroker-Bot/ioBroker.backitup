e94ecf1235d371b6036ce3f2b49112442ca27fe3
simatec <simatec@simateccloud.de>
2026-08-04 22:03:08 +0200

    (simatec) Update admin and tab build files

--- admin/custom/assets/mf-entry-bootstrap-0-f19cef4e.js
+++ admin/custom/assets/mf-entry-bootstrap-0-f19cef4e.js
@@ -26,11 +26,11 @@ const __mfImport = (src) =>
 
 
 (async () => {
-  const __mfHostInit = await __mfImport("./hostInit-BPePuRMy.js");
+  const __mfHostInit = await __mfImport("./hostInit-CjArDRDI.js");
   await __mfHostInit.__tla;
   const { initHost } = __mfHostInit;
   await initHost();
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-BMLXKv1J.js"));
+})().then(() => __mfImport("./index-CrK3xrYA.js"));
